6779376e58c2a42a5ac21684762fecb64a1eacfc
ivan_maleshko <maleshko.ivan@student.uzhnu.edu.ua>
2026-05-24 16:00:20 +0300

    fix: auto scroll on top of page

--- frontend/src/layout/Layout/Layout.tsx
+++ frontend/src/layout/Layout/Layout.tsx
@@ -1,9 +1,16 @@
 import styles from "./Layout.module.css";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Header } from "@/layout/Header/Header";
 import { Footer } from "@/layout/Footer/Footer";
+import { useEffect } from "react";
 
 export const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className={styles.wrapper}>
       <Header />
